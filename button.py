246abcae4211a8037ae56a90b2a0b4d64feb54b8
SLIME <165475965+SLIMEvoid45@users.noreply.github.com>
2024-04-20 17:22:57 +0500

    Update button.py

--- button.py
+++ button.py
@@ -1,28 +1,113 @@
 import pygame
+from pygame.locals import *
+
+pygame.init()
 
 
-class Button():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
-		self.clicked = False
+screen_width = 600
+screen_height = 600
 
-	def draw(self, surface):
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Button Demo')
+
+font = pygame.font.SysFont('Constantia', 30)
+
+#define colours
+bg = (204, 102, 0)
+red = (255, 0, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+#define global variable
+clicked = False
+counter = 0
+
+class button():
+		
+	#colours for button and text
+	button_col = (255, 0, 0)
+	hover_col = (75, 225, 255)
+	click_col = (50, 150, 255)
+	text_col = black
+	width = 180
+	height = 70
+
+	def __init__(self, x, y, text):
+		self.x = x
+		self.y = y
+		self.text = text
+
+	def draw_button(self):
+
+		global clicked
 		action = False
+
+		#get mouse position
 		pos = pygame.mouse.get_pos()
 
-
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
+		#create pygame Rect object for the button
+		button_rect = Rect(self.x, self.y, self.width, self.height)
+		
+		#check mouseover and clicked conditions
+		if button_rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1:
+				clicked = True
+				pygame.draw.rect(screen, self.click_col, button_rect)
+			elif pygame.mouse.get_pressed()[0] == 0 and clicked == True:
+				clicked = False
 				action = True
+			else:
+				pygame.draw.rect(screen, self.hover_col, button_rect)
+		else:
+			pygame.draw.rect(screen, self.button_col, button_rect)
+		
+		#add shading to button
+		pygame.draw.line(screen, white, (self.x, self.y), (self.x + self.width, self.y), 2)
+		pygame.draw.line(screen, white, (self.x, self.y), (self.x, self.y + self.height), 2)
+		pygame.draw.line(screen, black, (self.x, self.y + self.height), (self.x + self.width, self.y + self.height), 2)
+		pygame.draw.line(screen, black, (self.x + self.width, self.y), (self.x + self.width, self.y + self.height), 2)
 
-		if pygame.mouse.get_pressed()[0] == 0:
-			self.clicked = False
-
-		surface.blit(self.image, (self.rect.x, self.rect.y))
-
+		#add text to button
+		text_img = font.render(self.text, True, self.text_col)
+		text_len = text_img.get_width()
+		screen.blit(text_img, (self.x + int(self.width / 2) - int(text_len / 2), self.y + 25))
 		return action
+
+
+
+again = button(75, 200, 'Play Again?')
+quit = button(325, 200, 'Quit?')
+down = button(75, 350, 'Down')
+up = button(325, 350, 'Up')
+
+
+run = True
+while run:
+
+	screen.fill(bg)
+
+	if again.draw_button():
+		print('Again')
+		counter = 0
+	if quit.draw_button():
+		print('Quit')
+	if up.draw_button():
+		print('Up')
+		counter += 1
+	if down.draw_button():
+		print('Down')
+		counter -= 1
+
+	counter_img = font.render(str(counter), True, red)
+	screen.blit(counter_img, (280, 450))
+
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False	
+
+
+	pygame.display.update()
+
+
+pygame.quit()
